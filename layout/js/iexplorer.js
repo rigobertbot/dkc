4491d5246fae838be9365974fafe2e7fd4f16936
ElectricHound <ElectricHound@gmail.com>
2011-02-12 12:52:59 +0000

    Последняя (0.42) версия кода для сайта Детского кризисного центра, разрабатываемая на сервере dkz.neezyl.com

--- layout/js/iexplorer.js
+++
@@ -1,8 +0,0 @@
-jsHover = function() {
-		var hEls = document.getElementById("menu").getElementsByTagName("li");
-		for (var i=0, len=hEls.length; i<len; i++) {
-		hEls[i].onmouseover=function() { this.className+=" jshover"; }
-		hEls[i].onmouseout=function() { this.className=this.className.replace(" jshover", ""); }
-		}
-		}
-		if (window.attachEvent && navigator.userAgent.indexOf("Opera")==-1) window.attachEvent("onload", jsHover);
